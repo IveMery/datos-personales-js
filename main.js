3529f5ba1e92acf91e42ef46cdf683d3a90e6ea7
IvetteMery <ivette.mery@gmail.com>
2020-08-11 16:45:38 -0400

    ejercicio02js

---
+++ main.js
@@ -0,0 +1,6 @@
+alert("Para ingresalo al sistema por favor complete sus datos")
+let datosUsuario = prompt("Ingrese nombre")
+ + " , " + prompt("Ingrese su apellido")
++ " , " + prompt("Ingrese su edad") + " , " +  prompt("Ingrese nacionalidad") + " , " +  prompt("Ingrese documento")
+
+alert(`Nuevo Usuario agregado al sistema ${datosUsuario} `)
